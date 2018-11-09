5288fcf5c5bb827424b9371c99eb4ad3600b9c4c
Alabee <ianmutevu96@gmail.com>
2018-11-10 02:57:29 +0300

    Jirani and Donor tables created

--- ussd/models.py
+++ ussd/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Donor(models.Model):
+	phoneNumber = models.CharField(max_length=10)
+	amount = models.CharField(max_length=20)
+
+class Jirani(models.Model):
+	firstName = models.CharField(max_length=20)
+	lastName = models.CharField(max_length=20)
+	ID = models.CharField(max_length=20)
+	DOB = models.CharField(max_length=20)
+	phoneNumber = models.CharField(max_length=20)
+	county = models.CharField(max_length=20)
+	town = models.CharField(max_length=20)
+	estate/village = models.CharField(max_length=20)
+	landmark = models.CharField(max_length=20)
+	piecesRequested = models.CharField(max_length=20)
