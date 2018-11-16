52e1d69f63e1ef6815dc6846f487b56074163e62
Alabee <ianmutevu96@gmail.com>
2018-11-16 16:57:44 +0300

    Web setup

--- femijirani/settings.py
+++ femijirani/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['72660c03.ngrok.io', '127.0.0.1']
 
 INSTALLED_APPS = [
     'ussd',
+    'web',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
