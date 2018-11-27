95b43d527a4ee028a91623f5332049f8d21ea57d
Alabee <ianmutevu96@gmail.com>
2018-11-27 14:53:34 +0300

    Minor fixes

--- web/views.py
+++ web/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Homepage")
+	return render(request, 'web/home.html')
 
 def donate(request):
 	return HttpResponse("Donate page working!")
