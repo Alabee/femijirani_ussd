b8cce15f085620eb6f51e27e2e69b716cf4e9e13
Alabee <ianmutevu96@gmail.com>
2018-12-01 14:19:45 +0300

    Minor server setup

--- femijirani/settings.py
+++ femijirani/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zuby1+f^2+iaz&76v=$*d$4$+&#-g7i#l@=0bdnh=b+=hygnp*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['65c233ec.ngrok.io', '10.1.0.10']
+ALLOWED_HOSTS = ['65c233ec.ngrok.io', '127.0.0.1']
 
 
 # Application definition
