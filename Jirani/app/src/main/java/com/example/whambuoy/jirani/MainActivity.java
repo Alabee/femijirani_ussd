148970dca1e578f694acf391b958b1f24e32060a
Whambuoy <esther25maina@gmail.com>
2018-12-01 13:08:40 +0300

    App added

---
+++ Jirani/app/src/main/java/com/example/whambuoy/jirani/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.whambuoy.jirani;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    EditText name;
+    EditText pass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = (EditText) findViewById(R.id.username);
+        pass = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateCredentials();
+            }
+        });
+    }
+
+    public void validateCredentials(){
+        Toast.makeText(this, "Good!", Toast.LENGTH_SHORT).show();
+    }
+}
